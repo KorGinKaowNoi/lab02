26f7c1c09e9bde3961cced169c9b4e6ecb54f060
GetThisDonut <thitisan_ch@elearning.cmu.ac.th>
2021-07-08 18:06:31 +0700

    use computed

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ const app = Vue.createApp({
         return {
             product: 'Shoes',
             image: './assets/images/socks_green.jpg',
-            inStock: false ,
+            inStock: true ,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity:50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity:0 }
             ],
-            cart: 0
+            selected_item:0,
+            cart: 0,
+            brand:"donut"
 
         }
     },
@@ -19,7 +21,22 @@ const app = Vue.createApp({
             this.cart += 1
         },
         updateImage(variantImage) {
+            variantImage = this.variants[variantImage].image;
             this.image = variantImage
+        },
+        updateVariant(index){
+            this.selected_item=index
+        }
+    },
+    computed:{
+        title(){
+            return this.brand+" "+this.product
+        },
+        image(){
+            return this.variants[this.selected_item].image;
+        },
+        inStock(){
+            return this.variants[this.selected_item].quantity;
         }
     }
 })
